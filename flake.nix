c6ce4ee5624f71ec573b67594621d60a21f59a24
Curt Bushko <cbushko@gmail.com>
2026-06-20 16:38:11 -0400

    chore: add make wrapper for task compatibility

--- flake.nix
+++ flake.nix
@@ -40,6 +40,9 @@
       devShells = forEachSupportedSystem ({ pkgs, system }:
         let
           sharedConfigs = golang-shared-configs.packages.${system}.all-configs;
+          make-wrapper = pkgs.writeShellScriptBin "make" ''
+            exec ${pkgs.go-task}/bin/task "$@"
+          '';
         in {
         default = pkgs.mkShell {
           packages = with pkgs; [
@@ -50,6 +53,7 @@
             gotools
             golangci-lint
             (go-ai-lint { inherit pkgs; })
+            make-wrapper
             sharedConfigs
           ];
 
